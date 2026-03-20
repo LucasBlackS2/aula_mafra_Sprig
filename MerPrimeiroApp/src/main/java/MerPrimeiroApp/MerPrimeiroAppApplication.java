0a8e147b8ee7080a1699f3037e651cf1e790de3d
LucasBlackS2 <Lucas248s@hotmail.com>
2026-03-20 20:47:25 -0300

    att.

--- MerPrimeiroApp/src/main/java/MerPrimeiroApp/MerPrimeiroAppApplication.java
+++ MerPrimeiroApp/src/main/java/MerPrimeiroApp/MerPrimeiroAppApplication.java
@@ -1,20 +1,43 @@
-package MerPrimeiroApp;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class MerPrimeiroAppApplication {
-	@RequestMapping("/hello")
-	public String criaContato(){
-		return "Hello World";
+public class PrimeiroProjetoSpringApplication {
 
+	@PostMapping("/contato")
+	public String criarContato() {
+		return " Contato criado com sucesso !";
 	}
+
+
+	@GetMapping("/visualizar")
+	public String verContato() {
+		return " Contato visualizado com sucesso !";
+	}
+
+	@DeleteMapping("/deletar")
+	public String deletarContato() {
+		return "Contato excluido com sucesso !";
+	}
+
+	@PutMapping("/atualizar")
+	public String atualizarContato() {
+		return "Contato atualizado com sucesso !";
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(MerPrimeiroAppApplication.class, args);
+		SpringApplication.run(PrimeiroProjetoSpringApplication.class, args);
 	}
 
 }
